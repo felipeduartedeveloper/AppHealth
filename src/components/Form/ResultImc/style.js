053ae7f93e11db1253b8a62de51f39a1710e9e8d
Felipe duarte silva <felipesilvapac@gmail.com>
2021-06-24 15:51:23 -0300

    Primeiro commit

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+// import { Container } from './styles';
+
+const styles = StyleSheet.create({
+  ResultImc: {
+    flex:1,
+    marginTop:15,
+    paddingTop:60,
+    borderRadius:50,
+    alignItems: 'center',
+    width:"100%",
+    
+  },
+  numberImc:{
+    fontSize:48,
+    color:"#FF0048",
+    fontWeight:"bold",
+  },
+  information:{
+    fontSize:18,
+    color:"#FF0048",
+    fontWeight:"bold",
+  }
+})
+
+export default styles
